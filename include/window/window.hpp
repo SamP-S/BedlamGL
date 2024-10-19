bd4cfaa14ea0d5d5283c176c97fc4b984886f344
SamP-S <sam.ps1@outlook.com>
2024-10-19 19:40:38 +0100

    rm sdl2 deps from abstract window module

--- include/window/window.hpp
+++ include/window/window.hpp
@@ -1,12 +1,5 @@
 #pragma once
 
-// must define before SDL_opengl.h otherwise GLEW breaks
-#define GL_VERSION_4_4
-#include <GL/glew.h>
-
-#include <SDL2/SDL.h>
-#include <SDL2/SDL_opengl.h>
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <map>
@@ -14,32 +7,15 @@
 #include <iostream>
 
 #include "core/module.hpp"
-#include "input/input.hpp"
 
 namespace marathon {
 
 namespace window {
 
-#define WINDOW_WIDTH 800
-#define WINDOW_HEIGHT 600
-
-//// TODO:
-// Abstract renderer to allow for Vulkan, use cfg structs
-// Abstract so not SDL specific implementation
-// Implement all the window  property get/set
-// Rm event handling from here should be put in event class
-// Add support for OpenGL 4.6
-// Convert opengl config to use bools
-
-//// NOTE:
-// can add sRGB to OpenGLConfig
-// can add SDL_GL_CONTEXT_DEBUG_FLAG for better OpenGL debugging
-
-
 struct WindowConfig {
-    std::string title = "Config Title";
-    int minWidth = WINDOW_WIDTH;
-    int minHeight = WINDOW_HEIGHT;
+    std::string title = "Untitled Window";
+    int minWidth = 800;
+    int minHeight = 600;
     int width = 1280;
     int height = 720;
     int resizable = 1;
@@ -48,64 +24,31 @@ struct WindowConfig {
     int fullscreen = 0;
 };
 
-struct OpenGLConfig {
-    int major = 3;
-    int minor = 3;
-    const char* glsl = "#version 330 core";
-    int vsync = 1;
-    int doubleBuffering = 1;
-    int msaa = 1;
-    int msaaSamples = 4;
-    int depth = 24;
-    int stencil = 8;
-};
-
 class Window : public marathon::Module {
-private:
-    OpenGLConfig _openglConfig;
-    WindowConfig _windowConfig;
-    SDL_GLContext _openglContext;
-    SDL_Window* _window;
-    bool _isOk = false;
-    bool _isOpen = false;
-    std::vector<std::function<void(SDL_Event&)>> _eventHandlers;
-
 protected:
-    Window() = default;
-    ~Window() = default;
-
+    Window(const std::string& name);
+    
 public:
-    Window(const Window&) = delete;
-    Window& operator=(const Window&) = delete;
+    virtual ~Window();
+
     static Window& Instance();
     
     // module interface
-    void Boot() override;
-    void Shutdown() override;
-    std::string GetName() override;
-    marathon::ModuleType GetType() override;
+    // void Boot() override;
+    // void Shutdown() override;
 
     // common
-    void* GetGLContext();
-    SDL_Window* GetWindow();
-    // Add event handler for general context event
-    void AddEventHandler(std::function<void(SDL_Event&)> handler);
-    void PollEvents();
-    void SwapFrame();
-    bool IsOpen();
-    bool Close();
-    bool IsOk();
-
-    // readonly
-    OpenGLConfig GetOpenGLConfig();
-
+    virtual void SwapFrame();
+    
     // properties
-    void SetWindowMinSize(int minWidth, int minHeight);
-    void GetWindowMinSize(int& minWidth, int& minHeight);
-    void SetWindowSize(int width, int height);
-    void GetWindowSize(int& width, int& height);
-    void SetCursorCapture(bool capture);
-    bool GetCursorCapture();
+    virtual void SetWindowMinSize(int minWidth, int minHeight);
+    virtual void GetWindowMinSize(int& minWidth, int& minHeight);
+    virtual void SetWindowSize(int width, int height);
+    virtual void GetWindowSize(int& width, int& height);
+    virtual void SetCursorCapture(bool capture);
+    virtual bool GetCursorCapture();
+    virtual bool IsOpen();
+    virtual bool Close();
 };
 
 } // window
