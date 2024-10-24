2cbfb265b48474735b18fd1dbcfc83364d2aa7ad
SamP-S <sam.ps1@outlook.com>
2024-10-24 16:10:58 +0100

    impl base mesh class

---
+++ include/renderer/mesh.hpp
@@ -0,0 +1,158 @@
+#pragma once
+
+// PUBLIC HEADER
+
+#include <vector>
+#include <string>
+#include <utility>
+#include <memory>
+#include <cstdlib>
+
+#include "renderer/buffer.hpp"
+#include "renderer/drawable.hpp"
+
+namespace marathon {
+
+namespace renderer {
+
+static const std::vector<LA::vec3> defaultTriangleVertices = {
+    {  0.0f,  1.0f, 0.0f },
+    { -1.0f, -1.0f, 0.0f },
+    {  1.0f, -1.0f, 0.0f }
+};
+
+static const std::vector<LA::vec3> defaultQuadVertices = {
+    { -1.0f, -1.0f, 0.0f },
+    {  1.0f, -1.0f, 0.0f },
+    {  1.0f,  1.0f, 0.0f },
+    { -1.0f,  1.0f, 0.0f }
+};
+static const std::vector<uint32_t> defaultQuadIndices = {
+    0, 1, 2, 2, 3, 0
+};
+
+static const std::vector<LA::vec3> defaultCubeVertices = {
+    // front
+    { -1.0f, -1.0f,  1.0f },
+    {  1.0f, -1.0f,  1.0f },
+    {  1.0f,  1.0f,  1.0f },
+    { -1.0f,  1.0f,  1.0f },
+    // back
+    { -1.0f, -1.0f, -1.0f },
+    {  1.0f, -1.0f, -1.0f },
+    {  1.0f,  1.0f, -1.0f },
+    { -1.0f,  1.0f, -1.0f }
+};
+static const std::vector<uint32_t> defaultCubeIndices = {
+    0, 1, 2, 2, 3, 0,
+    1, 5, 6, 6, 2, 1,
+    7, 6, 5, 5, 4, 7,
+    4, 0, 3, 3, 7, 4,
+    3, 2, 6, 6, 7, 3,
+    4, 5, 1, 1, 0, 4
+};
+
+
+enum class PrimitiveType {
+    TRIANGLES,
+    FAN,
+    STRIP
+};
+
+enum class AttributeType {
+    // floating
+    HALF_FLOAT, // 16-bit
+    FLOAT,      // 32-bit
+    DOUBLE,     // 64-bit
+    // signed
+    INT8, 
+    INT16,
+    INT32,
+    // unsigned
+    UINT8,
+    UINT16,
+    UINT32
+};
+
+enum class IndexType {
+    UINT8,
+    UINT16,
+    UINT32,
+    NONE
+};
+
+struct VertexAttribute {
+    int index;
+    int numComponents;
+    AttributeType type;
+    int stride = 0;
+    int offset = 0;
+    bool _normalised = false;
+};
+
+/// TODO:
+// add support for setting a draw range (glDraw from start using count)
+// allow enable/disable attributes (glEnableVertexAttribArray/glDisableVertexAttribArray)
+// support a default data layout for default shaders
+// support changing vertex count after creation
+// fix access issues to prevent changing buffer data without updating vertex attributes
+// allow read access to vertex attributes
+// allow read access to index buffer
+
+// CONSIDER: allowing vertex format class and support changing vertex attributes after creation
+
+// HARD: add support for instanced rendering
+// HARD: add support for multiple vertex buffers for different vertex attributes
+//       probably requires the user to create buffers themsevles through the Renderer
+
+// NOTE: mesh vertex count is fixed once created
+//       mesh index map data can be resized/changed
+class Mesh : public Drawable {
+protected:
+    int _vertexCount = 0;
+    size_t _vertexSize = 0;
+    PrimitiveType _primitive = PrimitiveType::TRIANGLES;
+
+    std::shared_ptr<Buffer> _vBuffer = nullptr;
+    std::vector<VertexAttribute> _vAttrs = {};
+    std::shared_ptr<Buffer> _iBuffer = nullptr;
+    IndexType _indexType = IndexType::NONE;
+
+    // creates mesh with 0'ed vertex data
+    Mesh(const std::string& name, int vCount, size_t vSize, PrimitiveType primitive=PrimitiveType::TRIANGLES);
+    // create mesh with vertex data
+    Mesh(const std::string& name, int vCount, size_t vSize, std::shared_ptr<Buffer> vBuf, std::vector<VertexAttribute> vAttrs, 
+        PrimitiveType primitive=PrimitiveType::TRIANGLES);
+    // create mesh with vertex data and index map
+    Mesh(const std::string& name, int vCount, size_t vSize, std::shared_ptr<Buffer> vBuf, std::vector<VertexAttribute> vAttrs, 
+        std::shared_ptr<Buffer> iBuf, IndexType iType, PrimitiveType primitive=PrimitiveType::TRIANGLES);
+    virtual ~Mesh();
+
+    /// --- Backend Impl ---
+    virtual void Bind() = 0;
+    virtual void Unbind() = 0;
+    // drawable
+    virtual void Draw(Renderer& renderer, const LA::mat4& m) = 0;
+
+public:
+    /// --- Properties ---
+    PrimitiveType GetPrimitiveType() const;
+    void SetPrimitiveType(PrimitiveType primitive);
+
+
+    /// --- Index Data ---
+    // unsets index buffer
+    void SetIndexMap();
+    // set index buffer
+    void SetIndexMap(std::shared_ptr<Buffer> buf, IndexType type=IndexType::UINT32);
+    // get index buffer
+    // returns true if index buffer is set and fills args
+    // returns false if no index buffer set, args are set nullptr
+    // bool GetIndexMap(Buffer* buf, IndexType* type) const;
+    
+};
+
+} // renderer
+
+} // marathon
+
---
+++ src/renderer/mesh.cpp
@@ -0,0 +1,41 @@
+#include "renderer/mesh.hpp"
+
+namespace marathon {
+
+namespace renderer {
+
+/// TODO:
+// Add validation on data being input, accepts absolute shit atm, throw errors for the factor to catch
+
+// constructor/destructor
+Mesh::Mesh(const std::string& name, int vCount, size_t vSize, PrimitiveType primitive)
+    : Drawable(name),
+    _vertexCount(vCount), _vertexSize(vSize), _primitive(primitive) {}
+Mesh::Mesh(const std::string& name, int vCount, size_t vSize, std::shared_ptr<Buffer> vBuf, std::vector<VertexAttribute> vAttrs, PrimitiveType primitive)
+    : Drawable(name),
+    _vertexCount(vCount), _vertexSize(vSize), _primitive(primitive),
+    _vBuffer(vBuf), _vAttrs(vAttrs) {}
+Mesh::Mesh(const std::string& name, int vCount, size_t vSize, std::shared_ptr<Buffer> vBuf, std::vector<VertexAttribute> vAttrs, std::shared_ptr<Buffer> iBuf, IndexType iType, PrimitiveType primitive)
+    : Drawable(name),
+    _vertexCount(vCount), _vertexSize(vSize), _primitive(primitive),
+    _vBuffer(vBuf), _vAttrs(vAttrs),
+    _iBuffer(iBuf), _indexType(iType) {}
+
+Mesh::~Mesh() {}
+
+// properties
+PrimitiveType Mesh::GetPrimitiveType() const { return _primitive; }
+void Mesh::SetPrimitiveType(PrimitiveType primitive) { _primitive = primitive; }
+
+void Mesh::SetIndexMap() {
+    _iBuffer = nullptr;
+    _indexType = IndexType::NONE;
+}
+void Mesh::SetIndexMap(std::shared_ptr<Buffer> buf, IndexType type) {
+    _iBuffer = buf;
+    _indexType = type;
+}
+
+} // namespace renderer
+
+} // namespace marathon
