24f19d978ace7a0d676fa45a102566e34baabc41
SamP-S <sam.ps1@outlook.com>
2024-11-14 18:49:35 +0000

    add cube to test

--- test/no_driver/src/main.cpp
+++ test/no_driver/src/main.cpp
@@ -11,14 +11,16 @@ using namespace marathon;
 
 struct MyObject {
     LA::vec3 position = LA::vec3();
+    LA::vec3 rotation = LA::vec3();
     LA::vec4 color = LA::vec4({1.0f, 1.0f, 1.0f, 1.0f});
     std::shared_ptr<renderer::Mesh> mesh = nullptr;
 };
 
-MyObject _obj; MyObject _obj2;
+MyObject _obj; MyObject _obj2; MyObject _obj3;
 std::shared_ptr<renderer::Shader> _shader;
 std::shared_ptr<renderer::Mesh> _triangleMesh;
 std::shared_ptr<renderer::Mesh> _quadMesh;
+std::shared_ptr<renderer::Mesh> _cubeMesh;
 
 time::Time& Time = time::Time::Instance();
 window::Window& Window = window::Window::Instance();
@@ -53,6 +55,18 @@ void start() {
     _quadMesh->SetIndexParams(renderer::defaultQuadIndices.size(), renderer::IndexFormat::UINT32, renderer::PrimitiveType::TRIANGLES);
     _quadMesh->SetIndexData((void*)&renderer::defaultQuadIndices[0], renderer::defaultQuadIndices.size() * sizeof(uint32_t), 0, 0);
 
+    // create 3d mesh
+    _cubeMesh = std::make_shared<renderer::Mesh>();
+    _cubeMesh->SetVertexParams(
+        renderer::defaultCubeVertices.size(), 
+        {
+            {renderer::VertexAttribute::POSITION, 3, renderer::VertexAttributeFormat::FLOAT}
+        }
+    );
+    _cubeMesh->SetVertexData((void*)&renderer::defaultCubeVertices[0][0], renderer::defaultCubeVertices.size() * sizeof(LA::vec3), 0, 0);
+    _cubeMesh->SetIndexParams(renderer::defaultCubeIndices.size(), renderer::IndexFormat::UINT32, renderer::PrimitiveType::TRIANGLES);
+    _cubeMesh->SetIndexData((void*)&renderer::defaultCubeIndices[0], renderer::defaultCubeIndices.size() * sizeof(uint32_t), 0, 0);
+
     // create object
     _obj = MyObject();
     _obj.color = {1.0f, 0.2f, 0.2f, 1.0f};
@@ -62,6 +76,10 @@ void start() {
     _obj2 = MyObject();
     _obj2.color = {0.2f, 0.2f, 1.0f, 1.0f};
     _obj2.mesh = _quadMesh;
+
+    _obj3 = MyObject();
+    _obj3.color = {0.2f, 1.0f, 0.2f, 1.0f};
+    _obj3.mesh = _cubeMesh;
 
     std::string err = "";
     if (!Renderer.ValidateShader(_shader, err)) {
@@ -90,22 +108,37 @@ void update(double dt) {
     double time = Time.GetTime();
     _obj.position.x = 0.5f * sin(time);
     _obj2.position.y = 0.5f * cos(time * 2);
+    _obj3.rotation.y = time * 40.0f;
+    _obj3.rotation.x = time * 20.0f;
     // std::cout << "bedlam/include/runtime.hpp: time = " << time << " :: sin = " << sin(time) << std::endl;
 
     // make draw call of obj at position
     Renderer.Clear();
     Renderer.SetShader(_shader);
     if (_obj.mesh != nullptr) {
-        Renderer.PushScale({0.5f, 0.5f, 0.5f});
+        Renderer.PushScale({0.5f, 0.5f, 0.5f}); // scale down
+
+        // draw triangle
         Renderer.PushTranslate(_obj.position);
         Renderer.SetUniform("uTransform", Renderer.GetModel());
-        Renderer.Draw(_obj.mesh);
+        //Renderer.Draw(_obj.mesh);
         Renderer.PopTransform();
+
+        // draw quad
         Renderer.PushTranslate(_obj2.position);
         Renderer.SetUniform("uTransform", Renderer.GetModel());
-        Renderer.Draw(_obj2.mesh);
+        //Renderer.Draw(_obj2.mesh);
+        Renderer.PopTransform();
+
+        // draw cube
+        Renderer.PushRotate(_obj3.rotation);
+        Renderer.PushTranslate(_obj3.position);
+        Renderer.SetUniform("uTransform", Renderer.GetModel());
+        Renderer.Draw(_obj3.mesh);
         Renderer.PopTransform();
         Renderer.PopTransform();
+
+        Renderer.PopTransform();    // scale down
     } else {
         std::cout << "bedlam/include/runtime.hpp: test obj mesh is null" << std::endl;
     }
