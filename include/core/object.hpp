d002e8cee3cd5f3bbd4c0e5e27437302965b5be0
SamP-S <sam.ps1@outlook.com>
2024-11-25 20:15:28 +0000

    create base object class

---
+++ include/core/object.hpp
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <string>
+#include <memory>
+#include <iostream>
+#include "core/uuid.hpp"
+
+namespace marathon {
+
+/// An object is a base class for all instanitable classes (resources & nodes)
+/// TODO: implement UUID from file, UUID currently copies during copy
+class Object {
+private:
+    std::string _mType;
+    std::string _mName = "default_name";
+    UUID _mUUID = UUID();
+
+public:
+    Object() = default;
+    Object(const Object&) = default;
+    Object(const std::string& type="marathon.object");
+    ~Object() = default;
+
+    UUID GetUUID() const;
+    std::string GetName() const;
+    void SetName(const std::string& name);
+    std::string GetType() const;
+};
+
+} // marathon
